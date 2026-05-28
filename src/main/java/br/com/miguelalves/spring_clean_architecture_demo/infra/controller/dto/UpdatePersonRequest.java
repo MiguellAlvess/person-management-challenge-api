d18088bd5651637bb6ccafbf4998a8cb613413f9
Miguel Alves <miguellalvesiqueira@gmail.com>
2026-05-28 11:10:02 -0300

    feat: add UpdatePersonRequest dto

--- src/main/java/br/com/miguelalves/spring_clean_architecture_demo/infra/controller/dto/UpdatePersonRequest.java
+++ src/main/java/br/com/miguelalves/spring_clean_architecture_demo/infra/controller/dto/UpdatePersonRequest.java
@@ -6,47 +6,9 @@ import java.util.List;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 
-public class UpdatePersonRequest {
-    @NotBlank
-    private String name;
-
-    private LocalDate birthDate;
-
-    @NotBlank
-    private String cpf;
-
-    @Valid
-    private List<AddressRequest> addresses;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public List<AddressRequest> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<AddressRequest> addresses) {
-        this.addresses = addresses;
-    }
+public record UpdatePersonRequest(
+        @NotBlank String name,
+        LocalDate birthDate,
+        @NotBlank String cpf,
+        @Valid List<AddressRequest> addresses) {
 }
